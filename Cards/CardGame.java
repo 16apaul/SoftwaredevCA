37a91e3856476708aed3c6b794056d61d2870224
16apaul <86714317+16apaul@users.noreply.github.com>
2024-11-08 18:44:17 +0000

    Update CardGame.java

--- Cards/CardGame.java
+++ Cards/CardGame.java
@@ -4,50 +4,65 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class CardGame {
-    private int gameID;
-    private int numberOfPlayers;
-    public static String deckLocation = "Deck.txt";
+    private int gameID; // used to identify what game is running when multiple are
+    private int numberOfPlayers; // userinput
+    public static String deckLocation = "Deck.txt"; // Used in the test file and is default location
     ArrayList<String> bigDeck = BigDeck.loadDeck(deckLocation);
     private Deck deck;
     private Player player;
+    public static List<Player> players;
 
     public CardGame(int numberOfPlayers, String deckLocation ) {
         this.numberOfPlayers = numberOfPlayers;
-        this.deck = new Deck(1);
-        this.player = new Player(1, player.setStartingHand(), 1);
+        bigDeck.add(deckLocation); 
+        for (int i = 1; i <= numberOfPlayers; i++) { // i is the card preference and playerID 
+            List<Integer> startingHand = new ArrayList<>();
+
+            for (int j = 1; j <= 4; j++) { // inner loop for the getting the starting hand
+                startingHand.add(((j*numberOfPlayers)-(numberOfPlayers-1))+i-1); // starting hand gets assigned in a round robin fashion where each player takes every nth card from the deck 
+           
+            }
+            System.out.println(i+":current id");
+            player = new Player(i,i);
+            player.setStartingHand(startingHand);
+        }
     }
 
     public static void main(String[] args) {
         ArrayList<String> deck = BigDeck.loadDeck("Deck.txt");
 
-        // Print the ArrayList to verify contents
+        /*  Print the ArrayList to verify contents
         for (String line : deck) {
             System.out.println(line);
-        }
+        }*/
+        CardGame cardGame = new CardGame(2, deckLocation);
 
     }
 
 
     class Player {
-        private int PlayerID;
+        private int playerID; // starts at 1
         // deckOfDrawID
         // deckOfDiscardID
-        private List<Integer> startingHand = new ArrayList<Integer>();
+        private List<String> startingHand = new ArrayList<String>();
         private int cardPreference;
 
 
-        public Player(int playerID, List<Integer> startingHand, int cardPreference) {
+        public Player(int playerID, int cardPreference) {
 
 
             this.cardPreference = cardPreference;
-            this.PlayerID = playerID;
+            this.playerID = playerID;
         }
 
-        public List<Integer> setStartingHand() {
-            startingHand.add(1);
-            startingHand.add(1);
-            startingHand.add(1);
+        public List<String> setStartingHand( List<Integer> cards) {
+            for (int card : cards) {
+                startingHand.add(bigDeck.get(card));
+                System.out.println("Player ID:"+playerID+" got card"+ card);
+            }
+
             return startingHand;
 
         }
@@ -76,7 +91,6 @@ public class CardGame {
 
         }
     }
-
 
      class BigDeck {
 
